2013966a39018cdf7186ca3a9b590732c9bd48ed
Khaleeq <kahmad28@gmail.com>
2014-02-28 01:51:03 +0000

    Initialised tape for input, still issues with printing correct 1s and ^s in subsequent calls

--- turing_machine.rb
+++ turing_machine.rb
@@ -1,13 +1,18 @@
 class TuringMachine
-  $mult1
+
+  E = '\u2227'.gsub(/\\u[\da-f]{4}/i) { |m| [m[-4..-1].to_i(16)].pack('U') }      #empty character unicode
+  RIGHT = 'right'   #move right
+  LEFT = 'left'    #move left
+  HALT = 'halt'    #halt (don't move)
+
+
 
   def initialize
-
+    @multiplicand=0
+    @multiplier =0
+    @tape = E
   end
-  _ = '^'      #empty
-  r = 'right'   #move right
-  l = 'left'    #move left
-  h = 'halt'    #halt (don't move)
+
 
                        #current, new, direction, new state
 
@@ -29,33 +34,70 @@ class TuringMachine
   end
 
   def self.ask()
-    puts "Please enter a number"
-    #mult1 =
-        readline();
-   #puts mult1
+    puts "Please enter your first number (multiplicand)"
+    a = gets.chomp();
+
+    puts "Please enter the number you would like to multipy by (multiplier)"
+    b = gets.chomp();
+
+    @multiplicand = a.to_i
+    @multiplier   = b.to_i
   end
 
-  def self.addOnes(i)
-    ret = i.times{print "1"};
+  def self.printOnes(i)
+    chars = ''
+    i.times{chars << "#{1}" }
+    return chars
   end
+
+  def self.printEmpties(i)
+    chars = ''
+    i.times{chars << E}
+    return chars
+  end
+
+  #def self.printOnes(n)
+  #  ret = ''
+  #    for i in 1..n
+  #        ret << '1'
+  #    end
+  #  return ret
+  #end
+  #
+  #def self.printEmpties(n)
+  #  if n == nil
+  #    return E
+  #  else
+  #
+  #    ret =''
+  #    for i in 1..n
+  #      ret << E
+  #    end
+  #    return ret
+  #  end
+  #end
 
   rules = {
-           q0: {1 => rule(1, _, r, :q1), 0 => rule(_, _, l, :q9)},
-           q1: {1 => rule(1, 1, r, :q1), 0 => rule(_, _, r, :q2)},
-           q2: {1 => rule(1, _, r, :q3), 0 => rule(_, _, l, :q7)},
-           q3: {1 => rule(1, 1, r, :q3), 0 => rule(_, _, r, :q4)},
-           q4: {1 => rule(1, 1, r, :q4), 0 => rule(_, 1, l, :q5)},
-           q5: {1 => rule(1, 1, l, :q5), 0 => rule(_, _, l, :q6)},
-           q6: {1 => rule(1, 1, l, :q6), 0 => rule(_, 1, r, :q2)},
-           q7: {1 => rule(1, 1, l, :q7), 0 => rule(_, _, l, :q8)},
-           q8: {1 => rule(1, 1, l, :q8), 0 => rule(_, 1, r, :q0)},
-           q9: {1 => rule(1, 1, l, :q9), 0 => rule(_, _, r, :qF)},
-           qF: {1 => rule(1, 1, h, :qF)#, 0 => rule(_, _, h, :qF)
+           q0: {1 => rule(1, E, RIGHT, :q1), 0 => rule(E, E, LEFT, :q9)},
+           q1: {1 => rule(1, 1, RIGHT, :q1), 0 => rule(E, E, RIGHT, :q2)},
+           q2: {1 => rule(1, E, RIGHT, :q3), 0 => rule(E, E, LEFT, :q7)},
+           q3: {1 => rule(1, 1, RIGHT, :q3), 0 => rule(E, E, RIGHT, :q4)},
+           q4: {1 => rule(1, 1, RIGHT, :q4), 0 => rule(E, 1, LEFT, :q5)},
+           q5: {1 => rule(1, 1, LEFT, :q5), 0 => rule(E, E, LEFT, :q6)},
+           q6: {1 => rule(1, 1, LEFT, :q6), 0 => rule(E, 1, RIGHT, :q2)},
+           q7: {1 => rule(1, 1, LEFT, :q7), 0 => rule(E, E, LEFT, :q8)},
+           q8: {1 => rule(1, 1, LEFT, :q8), 0 => rule(E, 1, RIGHT, :q0)},
+           q9: {1 => rule(1, 1, LEFT, :q9), 0 => rule(E, E, RIGHT, :qF)},
+           qF: {1 => rule(1, 1, HALT, :qF)#, 0 => rule(E, E, h, :qF)
           }
   }
        #puts rules[:q0][1]
-  that = ask;
-  puts that.class
-  #addOnes(that);
+  @tape = E   # create empty tape (ie initial blank symbol)
+  ask()       # ask for input
+  product = (@multiplicand * @multiplier)
+  # append input to tape and print:
+        #  1s (* multiplicand)       +  separator       +  1s (* multiplier)
+  @tape << printOnes(@multiplicand) << E << printOnes(@multiplier)  #<< printEmpties(product)
+  puts @tape
 
 end
