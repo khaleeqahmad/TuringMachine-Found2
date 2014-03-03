c390d64d93255d34f17a200eea3c90e686dfadac
Khaleeq <kahmad28@gmail.com>
2014-03-03 03:59:50 +0000

    Formatted so prettier

--- turing_machine.rb
+++ turing_machine.rb
@@ -1,10 +1,14 @@
 class TuringMachine
+  require 'colorize'
   ONE = '1'
   EMPTY = '0'#\u2227'.gsub(/\\u[\da-f]{4}/i) { |m| [m[-4..-1].to_i(16)].pack('U') }      #empty character unicode
+  ELLIPSIS = '\u2026'.gsub(/\\u[\da-f]{4}/i) { |m| [m[-4..-1].to_i(16)].pack('U') }      #... character unicode
+
 
   RIGHT = 'right'   #move right
   LEFT = 'left'    #move left
   HALT = 'halt'    #halt (don't move)
+
 
   @states = ['q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'qF']
   STATES_LENGTH = @states.length - 1
@@ -79,6 +83,50 @@ class TuringMachine
     ruleRet(state, value, :state)
   end
 
+  def self.printHeader(max)
+    header =''
+    header = "Position:  "
+    for i in 0..(max)
+      case i
+        when 0
+          header << "  #{i}"
+        when 1..9
+          header << "   #{i}"
+        when 10..99
+          header << "  #{i}"
+        else #technically 100..999 for aesthetics, as any longer and the headings will go off position
+          header << " #{i}"
+      end
+    end
+    return header
+  end
+
+  def self.printTape(tape, lastPoint)
+    #.colorize(:color => :white, :background => :red)
+    printed = "[#{ELLIPSIS}|"
+    head, pos, tail = String.new
+
+    thisPoint = lastPoint +1 #slightly hacky way taking 'last'Point, as won't accept -1 (from thisPoint) for some reason??
+    nextPoint = thisPoint +1
+    head = tape.slice(0,lastPoint)
+    pos = tape.slice(thisPoint)
+    tail  = tape.slice((nextPoint), tape.length)
+
+    headPrint = "[#{ELLIPSIS}|"
+    tailPrint = ''
+
+    head.each_char do |i|
+      headPrint <<  ' '+i + ' |'
+    end
+
+    tail.each_char do |i|
+      tailPrint <<   '| ' + i+' '
+    end
+    tailPrint << "|#{ELLIPSIS}]"
+
+    print headPrint + " #{pos} ".colorize(:color => :white, :background => :red) + tailPrint
+  end
+
   def self.move(pos, direction)
     case direction
       when  LEFT
@@ -108,38 +156,30 @@ class TuringMachine
   @tape << EMPTY.dup   # create 'empty' tape (ie add initial blank symbol to array)
 
   #################### INPUT / INITIAL TAPE ###########################################
-  ask()           # ask for input
-  #  @multiplicand = 2
-  #  @multiplier    = 3
+  #ask()           # ask for input
+    @multiplicand = 2
+    @multiplier   = 3
 
   #find product
   @product = (@multiplicand * @multiplier)
+
+
 
   ## append input to tape and print:
     ones(@tape, @multiplicand) #add 1s for input multiplicand
     empties(@tape, 1)          #add seperator
     ones(@tape, @multiplier)   #add 1s for input multiplier
     empties(@tape, @product+2)   #add blank space ready for prospective product output
-  print @tape + "\n" #print tape as string
 
   length = @tape.length    #store tape length
 
+  puts printHeader(length-2)
+  print "[S:#{@states[0]}]\t"
+  printTape(@tape, 0)
+  print "\n" #print tape as string
+
+
 ####################################################################
-#  empty = 0
-#  one = 0
-  #@tape.each_with_index do |value, position|
-  #  state = :q0
-  #
-  #  if value == ONE
-  #    one += 1
-  #  else
-  #    empty+= 1
-  #  end
-  #
-  #end
-  #
-  #puts " ....  1s: #{one}, ^s:' #{empty}"
- ####################################################################
 
   state = 0       #start in start state (q0)
   pointer = 1       #set pointer to first char
@@ -154,13 +194,22 @@ class TuringMachine
 
     #update tape and print
       @tape[pointer] = newChar          #update character
-      print  @tape
+  print "[S: #{@states[state]}]\t"
+  #print "[P: #{[pointer]}]"
+
+
+  printTape(@tape, pointer)
+  print "\n"
 
     #make changes to progress
       state = newState                   #update state
       pointer = move(pointer, direction) #move pointer
 
-    print "[s:#{state}]\t\t\t"
   end
+  print "[s:#{@states[10]}]\t"
+  printTape(@tape, pointer)
+  print "\n" #print tape as string
+
+  #print "string".colorize(:color => :white, :background => :red)
 
 end
